bcc1ab0ceef13a499aa4f283d7b65c6f8e5f23b3
Kees van Welsenis <kvwelsenis@mopinion.com>
2021-03-26 18:49:50 +0100

    Replaced textfield package due to problems with RN >= 0.61

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -13,7 +13,7 @@ import {
   I18nManager,
 } from 'react-native';
 import Ripple from 'react-native-material-ripple';
-import { TextField } from '@ubaids/react-native-material-textfield';
+import { TextField } from 'rn-material-ui-textfield';
 
 import DropdownItem from '../item';
 import styles from './styles';
